c725578b9bfadaa2735d44eff1cb99e28f498b10
Zhansaya <63559079+soulzest@users.noreply.github.com>
2021-01-26 13:23:11 -0600

    more work in feature a code

--- src/test/java/featureACode.java
+++ src/test/java/featureACode.java
@@ -1,5 +1,6 @@
 public class featureACode {
     public static void main(String[] args) {
         System.out.println("feature a code");
+        System.out.println("B21 is learning git and review");
     }
 }
