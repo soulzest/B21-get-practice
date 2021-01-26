e8c64e1a8574e896bcbfce6586d244d1d319e253
Zhansaya <63559079+soulzest@users.noreply.github.com>
2021-01-26 14:26:59 -0600

    changes on line6

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         System.out.println("Hello world, B21");
         System.out.println("B21 future SDET learning Git");
         System.out.println("ready to move back");
+        System.out.println("dsd");
     }
 
 
