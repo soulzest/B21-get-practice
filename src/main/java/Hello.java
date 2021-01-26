5570274b21ed5cf7e5da372e6e1164d4ed58c7ce
Zhansaya <63559079+soulzest@users.noreply.github.com>
2021-01-26 14:15:59 -0600

    changes on line 4

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world, B21");
         System.out.println("B21 future SDET learning Git");
     }
+
+
 }
