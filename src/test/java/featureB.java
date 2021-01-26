07cc1afdfbc3fcfe4bef514b1e5c51696de9bbc0
Zhansaya <63559079+soulzest@users.noreply.github.com>
2021-01-26 13:34:39 -0600

    Revert "added some awesome test"

--- src/test/java/featureB.java
+++ src/test/java/featureB.java
@@ -1,5 +1,8 @@
 public class featureB {
     public static void main(String[] args) {
         System.out.println("Feature b");
+        System.out.println("More work");
+        System.out.println("More more work");
+
     }
 }
